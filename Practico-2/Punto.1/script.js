98dfd475655c9fa0c551f700a41e1a34d28a4ada
ezkiel18 <ezelionel011@gmail.com>
2025-04-25 18:22:08 -0300

    Commit 3

--- Practico-2/Punto.1/script.js
+++ Practico-2/Punto.1/script.js
@@ -1,14 +1,11 @@
-// Selecciona el título por ID y cambia su texto
 let titulo = document.getElementById("tituloPrincipal");
 titulo.textContent = "Nuevo título cambiado por JavaScript";
 
-// Selecciona los párrafos por clase y cambia su color
 let parrafos = document.getElementsByClassName("parrafo");
 for (let i = 0; i < parrafos.length; i++) {
   parrafos[i].style.color = "blue";
 }
 
-// Selecciona todos los <li> y agrega texto al final de cada uno
 let elementosLista = document.querySelectorAll("#contenedor li");
 elementosLista.forEach((li, index) => {
   li.textContent += ` (ítem ${index + 1})`;
